ec3cbd4dc431ee24d011df55c4be93bf3065d145
Joscelyn <JoscelynRose@gmail.com>
2019-06-23 17:39:31 -0400

    WIP: digital clock

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+
+const hourTenDigit = document.querySelector('hour-ten-digit');
+const hourOneDigit = document.querySelector('hour-one-digit');
+const minuteTenDigit = document.querySelector('minute-ten-digit');
+const minuteOneDigit = document.querySelector('minute-one-digit');
+const secondTenDigit = document.querySelector('second-ten-digit');
+const secondOneDigit = document.querySelector('second-one-digit');
+
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
+    console.log(seconds);
+    //secondOneDigit.innerText(seconds[1]);
+    //secondTenDigit.innerText(seconds[0]);
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
